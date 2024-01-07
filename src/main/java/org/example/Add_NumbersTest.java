c1d6d94c4921126a7661040bcffe9a12b8f94d4e
milan <niroulamilan>
2024-01-07 19:57:15 +0530

    making test case failed

--- src/main/java/org/example/Add_NumbersTest.java
+++ src/main/java/org/example/Add_NumbersTest.java
@@ -2,11 +2,16 @@ package org.example;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
 public class Add_NumbersTest {
 
     @Test
     public void AddNumbersTest() {
 
-        Assert(AddNumbers("123"), 6);
+        Add_Numbers caller = new Add_Numbers();
+
+        assertEquals(caller.sum("123"),6);
     }
 }
